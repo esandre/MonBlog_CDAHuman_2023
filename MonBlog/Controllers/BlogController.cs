837c241dae23bd798bd7319e84d3562466e763a8
Enzo Sandré <atelier@enzosandre.fr>
2023-10-18 16:33:38 +0200

    Nettoyage

--- MonBlog/Controllers/BlogController.cs
+++ MonBlog/Controllers/BlogController.cs
@@ -2,43 +2,42 @@ using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using MonBlog.Ports;
 
-namespace MonBlog.Controllers
+namespace MonBlog.Controllers;
+
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class BlogController : ControllerBase
+    private readonly IArticlesRepository _articlesRepository;
+
+    public BlogController(IArticlesRepository articlesRepository)
     {
-        private readonly IArticlesRepository _articlesRepository;
+        _articlesRepository = articlesRepository;
+    }
 
-        public BlogController(IArticlesRepository articlesRepository)
-        {
-            _articlesRepository = articlesRepository;
-        }
+    [HttpGet("/")]
+    public IActionResult Get()
+    {
+        return Content("<html><body>" +
+                       "<h1>Hello, World</h1>" +
+                       "</body></html>", "text/html");
+    }
 
-        [HttpGet("/")]
-        public IActionResult Get()
-        {
-            return Content("<html><body>" +
-                           "<h1>Hello, World</h1>" +
-                           "</body></html>", "text/html");
-        }
+    [HttpGet("/articles")]
+    public IActionResult GetArticles()
+    {
+        var articles = _articlesRepository.FetchAllArticles();
 
-        [HttpGet("/articles")]
-        public IActionResult GetArticles(int limit = 20)
-        {
-            var articles = _articlesRepository.FetchAllArticles();
-            var listItems = articles.Select(
-                article => $"<li><a href=\"/articles/{article.Permalink}\">{article.Titre}</a></li>"
-            );
+        var listItems = articles.Select(
+            article => $"<li><a href=\"/articles/{article.Permalink}\">{article.Titre}</a></li>"
+        );
 
-            return Content("<html><head>" +
-                           $"<meta charset=\"{Encoding.Default.BodyName}\">" +
-                           "</head><body>"
-                           + "<div id=\"articles\">" 
-                           + $"<ul>{string.Concat(listItems)}</ul>" 
-                           + "</div>"
-                           + "</body></html>",
-                "text/html");
-        }
+        return Content("<html><head>" +
+                       $"<meta charset=\"{Encoding.Default.BodyName}\">" +
+                       "</head><body>"
+                       + "<div id=\"articles\">" 
+                       + $"<ul>{string.Concat(listItems)}</ul>" 
+                       + "</div>"
+                       + "</body></html>",
+            "text/html");
     }
 }
