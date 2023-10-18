d00b506d30f38446f7024e22a15cb8ea022d13f2
Enzo Sandré <atelier@enzosandre.fr>
2023-10-18 15:19:53 +0200

    Ajout d'un connecteur Sqlite

---
+++ MonBlog.Sqlite/SqliteArticlesRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.Sqlite;
+using MonBlog.Models;
+using MonBlog.Ports;
+
+namespace MonBlog.Sqlite
+{
+    public class SqliteArticlesRepository : IArticlesRepository
+    {
+        private SqliteConnection _connection = new (":memory:");
+
+        /// <inheritdoc />
+        public IEnumerable<Article> FetchAllArticles()
+        {
+            try
+            {
+                _connection.Open();
+                using var command = _connection.CreateCommand();
+                command.CommandText = "SELECT title FROM articles";
+
+                var reader = command.ExecuteReader();
+
+                var articles = new List<Article>();
+
+                while (reader.HasRows)
+                {
+                    var titre = reader.GetString(0);
+                    articles.Add(new Article(titre));
+                }
+
+                return articles;
+            } 
+            finally
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
