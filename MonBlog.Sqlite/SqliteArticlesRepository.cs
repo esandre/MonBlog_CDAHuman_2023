63064a8106d6cef80776f2893830bac47aa64f2a
Enzo Sandré <atelier@enzosandre.fr>
2023-10-30 14:12:04 +0100

    [RED] Ajout d'une démo sur la protection des paramètres

--- MonBlog.Sqlite/SqliteArticlesRepository.cs
+++ MonBlog.Sqlite/SqliteArticlesRepository.cs
@@ -8,13 +8,20 @@ namespace MonBlog.Sqlite
         private SqliteConnection _connection = new (":memory:");
 
         /// <inheritdoc />
-        public IEnumerable<Article> FetchAllArticles()
+        public IEnumerable<Article> FetchAllArticles(string tag)
         {
             try
             {
                 _connection.Open();
                 using var command = _connection.CreateCommand();
-                command.CommandText = "SELECT title FROM articles";
+                command.CommandText = "SELECT title FROM articles WHERE tag = @tag";
+
+                var tagParameter = command.CreateParameter();
+                tagParameter.ParameterName = "tag";
+                tagParameter.SqliteType = SqliteType.Text;
+                tagParameter.Size = 32;
+                tagParameter.Value = tag;
+                command.Parameters.Add(tagParameter);
 
                 var reader = command.ExecuteReader();
 
